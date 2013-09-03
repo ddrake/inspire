b8608fbb9fb7ef408912a43af5a35a824e83946f
dow <dowdrake@msn.com>
2013-09-03 09:58:17 -0700

    updates

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ActiveRecord::Base
+
+  def self.find_next(message)
+    new_message = Message.where('id > ?', message.id).first
+    new_message = Message.first unless new_message
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Inspire::Application.routes.draw do
 
   resources :sessions
 
+  get 'inspirations/change' => 'inspirations#change'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
